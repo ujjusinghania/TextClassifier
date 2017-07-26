c20730846d620ddcd9e0f81b048a07ca88d51b72
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-07-26 12:42:42 +0530

    Initial implementation of the Neuroph Neural Network
    - Implemented an initial version of the neural network.
    - Created classes and methods to create the training data and testing
    data.
    - Added the required libraries.

--- src/textclassifier/NeuralNetworkTextClassifier.java
+++ src/textclassifier/NeuralNetworkTextClassifier.java
@@ -6,8 +6,14 @@
 package textclassifier;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.Perceptron;
 
@@ -17,15 +23,43 @@ import org.neuroph.nnet.Perceptron;
  */
 public class NeuralNetworkTextClassifier {
     
-    public double[] runNeuralNetwork() {
+    protected static void trainNeuralNetwork() throws IOException {
+        
+        NeurophNeuralNetworkDataCreator dataCreator = new NeurophNeuralNetworkDataCreator();
+        
+        HashMap<Double[], Double[]> dataArray = new HashMap<>();
+        String[] folders = {"tech", "sport", "business"};
+        dataArray = dataCreator.createDataDumpFromTxtFolder(folders); 
+        
+        System.out.println(dataArray.size());
+        int trainingDataSize = (int) (dataArray.size() * 0.85);
+        System.out.println(trainingDataSize);
+        
+        Set<Double[]> dataSet = dataArray.keySet();
+        HashMap<Double[], Double[]> testingDataSet = new HashMap<>();
+        
+        int inputSize = 0; 
+        int outputSize = 0;
+        
+        for (Double[] key: dataSet) {
+                inputSize = key.length;
+                outputSize = dataArray.get(key).length;
+                break;
+        }
+        
+        System.out.println(inputSize + " " + outputSize);
+        
         
         NeuralNetwork textClassificationNN = new MultiLayerPerceptron(0, 0);
-        
-        return null; 
+
     }
     
     public static void main(String[] args) {
-        
+        try { 
+            trainNeuralNetwork();
+        } catch (IOException ex) {
+            Logger.getLogger(NeuralNetworkTextClassifier.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
--- src/textclassifier/NeurophNeuralNetworkDataCreator.java
+++ src/textclassifier/NeurophNeuralNetworkDataCreator.java
@@ -5,16 +5,142 @@
  */
 package textclassifier;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.neuroph.core.data.DataSet;
+
 /**
  *
  * @author ujjwalsinghania
  */
 public class NeurophNeuralNetworkDataCreator {
-    
+
     TextUtilities textUtilities = new TextUtilities();
+
+    private Map<String, Double[]> classificationMap;
     
-   public void createDataDumpFromExcelSheet() {
-       
-   }
+/**
+    public HashMap<Double[], Double[]> createDataDumpFromExcelSheet(String fileName) throws IOException, InvalidFormatException {
+        try {
+
+            XSSFWorkbook wb = new XSSFWorkbook(new File("data/" + fileName));
+            XSSFSheet sheet = wb.getSheetAt(0);
+
+            Map<String, Integer> excelSheetDatabase = new HashMap<String, Integer>();
+            classificationMap = new HashMap<String, Double[]>();
+            Integer classTypeIdentifier = 1;
+
+            for (Row row : sheet) {
+                if (row.getRowNum() == 0) {
+                    continue;
+                }
+                String classType = row.getCell(4).toString();
+                if (classType == null) {
+                    continue;
+                }
+                if (classificationMap.containsKey(classType) == false) {
+                    classificationMap.put(classType, classTypeIdentifier);
+                    classTypeIdentifier += 1;
+                }
+                excelSheetDatabase.put(row.getCell(1).toString(), classificationMap.get(classType));
+            }
+
+            for (String key : excelSheetDatabase.keySet()) {
+                TreeMap<Integer, Integer> wordFrequencyMap = textUtilities.splitStringAndMakeWordFrequencyMap(key);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("File " + fileName + " couldn't be found: createDataDumpFromTxtFolder(String folder)");
+        }
+        return null;
+    }
+    */
     
+    private Double[] convertToBinaryArray(int classTypeIdentifier, int numberOfClasses) {
+        String[] bits = Integer.toBinaryString(classTypeIdentifier).split("");
+        Double[] bitPattern = new Double[numberOfClasses];
+        for (int index = 0; index < bits.length; index++) {
+            bitPattern[index] = Double.parseDouble(bits[index]);
+        }
+        return bitPattern;
+    }
+    
+    private Double[] convertToDoubleArray(TreeMap<Integer, Integer> textFile, int numberOfWords) {
+        Double[] wordArray = new Double[numberOfWords+1];
+        for (int key: textFile.keySet()) {
+            wordArray[key] = (double)textFile.get(key); 
+        }
+        return wordArray;
+    }
+    
+    public HashMap<Double[], Double[]> createDataDumpFromTxtFolder(String[] folders) throws FileNotFoundException, IOException {
+
+        HashMap<Double[], Double[]> dataFile = new HashMap<>();
+        ArrayList<TreeMap<Integer, Integer>> wordFrequencyMap = new ArrayList<>();
+        ArrayList<Double[]> classificationMapForDocument = new ArrayList<>(); 
+        
+        int classTypeIdentifier = 1;
+        classificationMap = new HashMap<String, Double[]>();
+
+        for (int i = 0; i < folders.length; i++) {
+
+            String folder = folders[i];
+
+            // Change path extension to location of working directory/folder containing all files.
+            File[] files = new File("data/" + folder).listFiles();
+
+            if (classificationMap.containsKey(folders[i]) == false) {
+                classificationMap.put(folders[i], convertToBinaryArray(classTypeIdentifier, folders.length));
+                classTypeIdentifier += 1;
+            }
+
+            for (File file : files) {
+                if (!file.isFile()) {
+                    continue;
+                }
+
+                String fileName = file.getAbsolutePath();
+                String fileLine;
+                String textFile = "";
+
+                try {
+
+                    FileReader fileReader = new FileReader(fileName);
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                    while ((fileLine = bufferedReader.readLine()) != null) {
+                        textFile += fileLine;
+                    }
+
+                    bufferedReader.close();
+
+                    wordFrequencyMap.add(textUtilities.splitStringAndMakeWordFrequencyMap(textFile));
+                    classificationMapForDocument.add(classificationMap.get(folder));
+
+                } catch (FileNotFoundException ex) {
+                    System.out.println("File " + fileName + " couldn't be found: createDataDumpFromTxtFolder(String folder)");
+                }
+
+            }
+        }
+        
+        int totalNumberOfWords = textUtilities.getNumberOfWords();
+        
+        for (int index = 0; index < classificationMapForDocument.size(); index++) {
+            dataFile.put(convertToDoubleArray(wordFrequencyMap.get(index), totalNumberOfWords), classificationMapForDocument.get(index));
+        }     
+        return dataFile; 
+    }
+
 }
